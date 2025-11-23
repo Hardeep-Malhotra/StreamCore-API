8874c367fd31f3e42cd9dcb2a46b8908d346eca9
Hardeep-Malhotra <hardeepmalhotra022@gmail.com>
2025-11-23 14:58:29 +0530

    Add ApiError and ApiResponse utilities

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import dotenv from 'dotenv';
 import connectDB from "./db/index.js";
 
 dotenv.config({
-      path:'./env'
+    path: './env'
 })
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port : ${process.env.PORT}`);
+            app.on("error", (error) => {
+                console.log("ERROR:", error);
+                throw err;
+            })
+        })
+    })
+    .catch((err) => {
+        console.log("MONGO DB CONNECTION FAILED !!", err);
 
+    })
 
 
 
